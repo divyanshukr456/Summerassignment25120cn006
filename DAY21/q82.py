69682966bb96bfaf940f1513e77bf73940191f69
Divyanshu Kumar Sharma <skylife4098@gmail.com>
2026-06-26 12:26:50 +0530

    Add files via upload

---
+++ DAY21/q82.py
@@ -0,0 +1,7 @@
+s = input("Enter a string: ")
+
+reverse = ""
+for i in s:
+    reverse = i + reverse
+
+print("Reversed String =", reverse)
---
+++ DAY21/q84.py
@@ -0,0 +1,11 @@
+s = input("Enter a string: ")
+
+result = ""
+
+for ch in s:
+    if 'a' <= ch <= 'z':
+        result += chr(ord(ch) - 32)
+    else:
+        result += ch
+
+print("Uppercase =", result)
